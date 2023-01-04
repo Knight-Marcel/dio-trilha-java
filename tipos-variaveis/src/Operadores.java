4e52a4014b7a472773bdc2457005664ddfe7679d
Marcel de Menezes Araujo <araujomarcel89@gmail.com>
2023-01-04 09:49:33 -0300

    operador ternario

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,3 +1,5 @@
+import javax.lang.model.util.ElementScanner14;
+
 public class Operadores {
     public static void main(String[] args) {
        /*
@@ -8,11 +10,33 @@ public class Operadores {
 
         numero =  numero * -1;
         System.out.println(numero);*/
-
+        /*
         int numero = 5;
         //repetição
         numero = numero + 1; // ++ é a  mesma coisa de numero +1
         System.out.println(numero);
+
+        boolean variavel = true;
+        System.out.println(!variavel);*/
+        
+        //operadores ternario
+        //utilizando por if else
+        int a , b ;
+        a= 6;
+        b=3;
+        String resultado = a==b ? "verdadeiro" : "falso";
+        /*
+        if(a==b)
+            resultado  = "verdadeiro";
+        else
+            resultado = "false";*/
+
+        
+        System.out.println(resultado);
+
+
+
+
 
 
 
