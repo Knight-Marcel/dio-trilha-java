6378b62e6b2640aef1671e6d818922706e38160a
Marcel de Menezes Araujo <araujomarcel89@gmail.com>
2023-01-04 09:37:30 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores {
+    public static void main(String[] args) {
+       /*
+        int numero = 5;
+        numero = - numero;
+        System.out.println(-numero);
+        System.out.println(numero);
+
+        numero =  numero * -1;
+        System.out.println(numero);*/
+
+        int numero = 5;
+        //repetição
+        numero = numero + 1; // ++ é a  mesma coisa de numero +1
+        System.out.println(numero);
+
+
+
+    }
+}
