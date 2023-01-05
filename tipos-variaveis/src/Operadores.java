979046c35ca9a58e323dcef161bc8a7be6325cd0
Marcel de Menezes Araujo <araujomarcel89@gmail.com>
2023-01-05 10:51:12 -0300

    operadores logicos com if

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -21,18 +21,29 @@ public class Operadores {
         
         //operadores ternario
         //utilizando por if else
-        int a , b ;
+      /*  int a , b ;
         a= 6;
         b=3;
-        String resultado = a==b ? "verdadeiro" : "falso"; //?: "operador ternario pode ser utilizado para outros tipos de dados além de string"
+        String resultado = a==b ? "verdadeiro" : "falso"; //?: "operador ternario pode ser utilizado para outros tipos de dados além de string "
         /*
         if(a==b)
             resultado  = "verdadeiro";
         else
-            resultado = "false";*/
+            resultado = "false";
 
         
-        System.out.println(resultado);
+        System.out.println(resultado);*/
+
+        boolean condicaoUm = true;
+        boolean condicaoDois=true;
+
+        if(condicaoUm && condicaoDois){
+            System.out.println("As duas condições são verdadeiras");
+        }
+        if (condicaoUm || condicaoDois){
+            System.out.println("Uma das condições é verdadeira");
+        }
+
 
 
 
