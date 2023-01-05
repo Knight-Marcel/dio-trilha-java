a3963191bbe1a70281412eb00aec4bf4b8e11aa7
Marcel de Menezes Araujo <araujomarcel89@gmail.com>
2023-01-05 12:09:44 -0300

    exercício de métodos Smartv

--- metodos/sistema-smart-tv/src/SmartTv.java
+++ metodos/sistema-smart-tv/src/SmartTv.java
@@ -3,6 +3,30 @@ public class SmartTv{
  int canal =1;
  int volume= 25;   
 
+public void aumentarVolume(){
+    volume++;
+    System.out.println("aumentando volume:  " + volume);
+}
+
+public void diminuirVolume(){
+    volume--;
+    System.out.println("diminuindo volume:  " + volume);
+}
+
+public void mudarCanal (int novoCanal){
+    canal= novoCanal;
+}
+
+public void aumentarCanal(){
+    canal++;
+    System.out.println("aumentando canal:  " + canal);
+}
+
+public void diminuirCanal(){
+    canal--;
+    System.out.println("diminuindo canal:  " + canal);
+}
+
 public void ligar (){
     ligada=true ;
 }
