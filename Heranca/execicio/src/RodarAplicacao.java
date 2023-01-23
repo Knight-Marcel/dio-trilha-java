667a6a75747a26fc79cea57a7ca5f874d65bc72f
Marcel de Menezes Araujo <araujomarcel89@gmail.com>
2023-01-23 15:12:18 -0300

    metodos em arraylist

---
+++ Heranca/execicio/src/RodarAplicacao.java
@@ -0,0 +1,15 @@
+public class RodarAplicacao {
+    public static void main(String[] args) {
+        
+        Funconario funcionario = new Funconario();
+
+        Funconario gerente = new Gerente();
+        Funconario vendedor = new Vendedor();
+        Funconario Faxineiro = new Faxineiro();
+
+
+
+
+
+    }
+}
